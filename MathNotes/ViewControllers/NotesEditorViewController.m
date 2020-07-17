51dfb638ac4026719c3c3ddbf8870010faca0bdd
RainbowMan1 <nikeshsubedi2@gmail.com>
2020-07-17 15:48:31 -0400

    Added rename note feature

--- MathNotes/ViewControllers/NotesEditorViewController.m
+++ MathNotes/ViewControllers/NotesEditorViewController.m
@@ -10,6 +10,7 @@
 #import "EquationPickerViewController.h"
 
 @interface NotesEditorViewController ()<EquationPickerDelegate>
+@property (strong, nonatomic) UIButton *noteNameButton;
 @end
 
 @implementation NotesEditorViewController
@@ -19,12 +20,12 @@
     // HTML Content to set in the editor
     self.formatHTML = NO;
     
-    UIButton *noteNamebutton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [noteNamebutton setTitle:self.note.noteName forState:UIControlStateNormal];
-    [noteNamebutton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [noteNamebutton addTarget:self action:@selector(changeNoteName) forControlEvents:UIControlEventTouchUpInside];
-    noteNamebutton.frame = CGRectMake(0, 0, self.view.frame.size.width, 64.0);
-    self.navigationItem.titleView =noteNamebutton;
+    self.noteNameButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.noteNameButton setTitle:self.note.noteName forState:UIControlStateNormal];
+    [self.noteNameButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.noteNameButton addTarget:self action:@selector(changeNoteName) forControlEvents:UIControlEventTouchUpInside];
+    self.noteNameButton.frame = CGRectMake(0, 0, self.view.frame.size.width, 64.0);
+    self.navigationItem.titleView =self.noteNameButton;
     
     [self setHTML:self.note.htmlText];
 }
@@ -37,15 +38,24 @@
 }
 
 - (void)changeNoteName {
-    self.note.noteName = @"changed Name";
-    UIButton *noteNamebutton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [noteNamebutton setTitle:self.note.noteName forState:UIControlStateNormal];
-    [noteNamebutton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [noteNamebutton addTarget:self action:@selector(changeNoteName) forControlEvents:UIControlEventTouchUpInside];
-    noteNamebutton.frame = CGRectMake(0, 0, self.view.frame.size.width, 64.0);
-    self.navigationItem.titleView =noteNamebutton;
-}
-- (IBAction)addEquation:(id)sender {
+    UIAlertController * alertController = [UIAlertController alertControllerWithTitle: @"Rename Note"
+                                                                                     message: @""
+                                                                                 preferredStyle:UIAlertControllerStyleAlert];
+       [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+           textField.placeholder = @"name";
+           textField.borderStyle = UITextBorderStyleRoundedRect;
+           textField.text = self.note.noteName;
+           textField.clearsOnInsertion =YES;
+       }];
+       [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+           NSArray * textfields = alertController.textFields;
+           UITextField * namefield = textfields[0];
+           NSLog(@"%@",namefield.text);
+           self.note.noteName = namefield.text;
+           [self.noteNameButton setTitle:self.note.noteName forState:UIControlStateNormal];
+       }]];
+       [self presentViewController:alertController animated:YES completion:nil];
+    
 }
 
 - (void)didPickEquationSnip:(nonnull EquationSnip *)equationSnip {
